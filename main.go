b557dfb291c68a3b2a879a67273802963b69fedf
Jim Male <2423859+jimmale@users.noreply.github.com>
2020-12-13 20:31:43 -0600

    stuff

--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
-import (
-	"github.com/jimmale/gobraviacontrol/braviacontrol"
-	"github.com/jimmale/gobraviacontrol/braviacontrol/powerstatus"
-	"net"
-	"time"
-)
+import "fmt"
 
 func main() {
-	display, _ := braviacontrol.NewDisplay(net.IP("192.168.1.42"), 8080)
-
-	_ = display.SetPowerStatus(powerstatus.POWER_ON)
-
-	time.Sleep(30 * time.Minute)
-
-	_ = display.SetPowerStatus(powerstatus.POWER_OFF)
-	display.Close()
+	fmt.Println("Hello, GoBraviaControl")
+	//display, _ := braviacontrol.NewDisplay(net.IP("192.168.1.42"), 8080)
+	//
+	//_ = display.SetPowerStatus(powerstatus.POWER_ON)
+	//
+	//time.Sleep(30 * time.Minute)
+	//
+	//_ = display.SetPowerStatus(powerstatus.POWER_OFF)
+	//display.Close()
 
 }
